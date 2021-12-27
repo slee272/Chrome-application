05623ecc6c01d76b34864246af65dea3eb77ffa3
slee272 <slee272@student.hywoman.ac.kr>
2021-12-27 17:21:16 +0900

    🎨 function에 argument 추가로 submit시 자동 새로고침 제어

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+  event.preventDefault();
   console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
